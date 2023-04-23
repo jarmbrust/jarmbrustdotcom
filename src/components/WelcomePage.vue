8b8fad6c8d2edb350ae7d7ae1957b594e40ebc70
James Armbrust <james.armbrust@gmail.com>
2023-04-23 11:55:19 -0600

    added project to github

---
+++ src/components/WelcomePage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="welcome">
+    This is a welcome page to test the deployments.
+  </div>
+</template>
+
+<style scoped>
+  .welcome {
+    margin: 30px;
+  }
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import WelcomePage from '../components/WelcomePage.vue'
+</script>
+
+<template>
+  <main>
+    <WelcomePage />
+  </main>
+</template>
