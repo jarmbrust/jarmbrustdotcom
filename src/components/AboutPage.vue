ebb18a986bbfe33a58ff9e563f42a16ce7e56510
James Armbrust <james.armbrust@gmail.com>
2023-04-30 16:06:59 -0600

    about edit

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -6,7 +6,7 @@ import PageHeader from './PageHeader.vue';
   <PageHeader title="About me..." />
   <div class="about">
     <p>
-      Currently reside with my wife and our zoo of animals in a small home
+      Currently my wife and I reside with our zoo of animals in a small home
       south of Denver Colorado.  I work as a software engineer and enjoy 
       TTRPGs, scifi shows, B-movies, reading, and occasionally attempting to
       sketch something.
